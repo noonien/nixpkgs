8623e0528ea5cd756428e86af654e6f9289fb9a6
George-Cristian Jiglau <george@mux.ro>
2021-03-17 14:23:47 +0200

    kicad-unstable: 2020-12-23 -> 2021-03-17

--- pkgs/applications/science/electronics/kicad/versions.nix
+++ pkgs/applications/science/electronics/kicad/versions.nix
@@ -27,23 +27,23 @@
   };
   "kicad-unstable" = {
     kicadVersion = {
-      version =			"2020-12-23";
+      version =			"2021-03-17";
       src = {
-        rev =			"912657dd238ad78cfc5d9d5e426ea850d5554fb3";
-        sha256 =		"1p5kr4d4zpajwdmya1f351y1ix8qmvsx1hrnvhzh7yc3g72kgxah";
+        rev =			"fee3c41c40a70a3ef865f4d83c999cbbc6f18a74";
+        sha256 =		"0kim830apprlxivb080wnl694z02w9p043rzqyzss980wlkrxjgw";
       };
     };
     libVersion = {
-      version =			"2020-12-23";
+      version =			"2021-03-17";
       libSources = {
         i18n.rev =		"e89d9a89bec59199c1ade56ee2556591412ab7b0";
         i18n.sha256 =		"04zaqyhj3qr4ymyd3k5vjpcna64j8klpsygcgjcv29s3rdi8glfl";
-        symbols.rev =		"e538abb015b4f289910a6f26b2f1b9cb8bf2efdb";
-        symbols.sha256 =	"117y4cm46anlrnw6y6mdjgl1a5gab6h6m7cwx3q7qb284m9bs5gi";
-        templates.rev =		"32a4f6fab863976fdcfa232e3e08fdcf3323a954";
-        templates.sha256 =	"13r94dghrh9slpj7nkzv0zqv5hk49s6pxm4q5ndqx0y8037ivmhk";
-        footprints.rev =	"15ffd67e01257d4d8134dbd6708cb58977eeccbe";
-        footprints.sha256 =	"1ad5k3wh2zqfibrar7pd3g363jk2q51dvraxnq3zlxa2x4znh7mw";
+        symbols.rev =		"3c1d197bf276d0b7cccb9490f12738f54889922d";
+        symbols.sha256 =	"17l4bxhn7iys56i0a32rm0rcxaa657miik31ldxr31pmpnk1yyhb";
+        templates.rev =		"073d1941c428242a563dcb5301ff5c7479fe9c71";
+        templates.sha256 =	"14p06m2zvlzzz2w74y83f2zml7mgv5dhy2nyfkpblanxawrzxv1x";
+        footprints.rev =	"964e997877b61806cb800d97e360a6c1bc5fab52";
+        footprints.sha256 =	"0i7f40mcwd90b447x64fb58xnpfvn08ai59sg5nm2rbp623dqpvp";
         packages3d.rev =	"d8b7e8c56d535f4d7e46373bf24c754a8403da1f";
         packages3d.sha256 =	"0dh8ixg0w43wzj5h3164dz6l1vl4llwxhi3qcdgj1lgvrs28aywd";
       };
